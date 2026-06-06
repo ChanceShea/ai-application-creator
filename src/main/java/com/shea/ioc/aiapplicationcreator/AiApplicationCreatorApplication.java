952a57fa7a0e82d13aac966f39746cb73a5cca73
CShea <2788456221@qq.com>
2026-06-06 14:00:54 +0800

    项目初始化

---
+++ src/main/java/com/shea/ioc/aiapplicationcreator/AiApplicationCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.shea.ioc.aiapplicationcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApplicationCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplicationCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shea/ioc/aiapplicationcreator/AiApplicationCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shea.ioc.aiapplicationcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
